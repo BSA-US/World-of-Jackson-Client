4c58c0be26cf96fff034ced8285ef663066eef49
Tyler Freeman <git@odbol.com>
2020-05-31 12:03:37 -0700

    Refactored TourModal out of TourBar

--- components/TourBar/TourBar.tsx
+++ components/TourBar/TourBar.tsx
@@ -4,25 +4,18 @@ import TourButton from './TourButton';
 
 import UITheme from 'styled-components'; 
 import { LngLat } from '~/pages';
-var sanitizeHtml: any = require('sanitize-html');
 
 const SideBar = UITheme.div`
     background-color: rgb(200, 200, 200);
-    position: absolute;
+    position: fixed;
     left: 0;
     top: 0;
     bottom: 0;
-`
-const InfoArea = UITheme.div`
-    background-color: rgb(240, 240, 240);
-    position: absolute;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    font-size: 0.8em;
-    line-height: 1em;
+    width: 80px;
+    padding: 8px;
 `
 
+// TODO(odbol): move this to a db/models directory?
 export interface ITourNode {
   label: string,
   description: string,
@@ -32,18 +25,10 @@ export interface ITourNode {
 
 const TourBar: FunctionComponent<{ tour: ITourNode[], handleTourClick: (tourNode: ITourNode) => void, selectedTourNode: ITourNode | null }> =
   ({ tour, handleTourClick, selectedTourNode }) => {
-      const sidebarWidth = '20px'
-
-      let description = selectedTourNode ? sanitizeHtml(selectedTourNode.description) : "";
-
       return (
-          <div>
-              <SideBar style={{ width: sidebarWidth }}>
-                {tour.map((node) => <TourButton tourNode={node} key={node.label} handleTourClick={ handleTourClick } selectedTourNode={ selectedTourNode }/>)}
-              </SideBar>
-              <InfoArea style={{ left: sidebarWidth }} dangerouslySetInnerHTML={{ __html: description }}>
-              </InfoArea>
-          </div>
+          <SideBar>
+            {tour.map((node) => <TourButton tourNode={node} key={node.label} handleTourClick={ handleTourClick } selectedTourNode={ selectedTourNode }/>)}
+          </SideBar>
       )
   }
 
